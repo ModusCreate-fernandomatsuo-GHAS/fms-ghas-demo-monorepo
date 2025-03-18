d72507a5c618901aea41be885c68bdf14cd39925
Fernando Matsuo Santos <fernando@familiamatsuo.com>
2025-03-18 20:02:18 -0300

    start GHAS demo

--- build_example/c_plus_plus/include/helper.h
+++
@@ -1,6 +0,0 @@
-#ifndef HELPER_H
-#define HELPER_H
-
-int helperFunction(int num, const std::string& fileName);
-
-#endif // HELPER_H
--- build_example/dotnet-typescript/backend/Models/SampleModel.cs
+++
@@ -1,7 +0,0 @@
-namespace my_dotnet_app.Models
-{
-    public class SampleModel
-    {
-        // Add properties and methods for the sample model here
-    }
-}
