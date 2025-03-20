f8aa087135818aee789dbbd9b2a121494e13af76
Fernando Matsuo santos <76171689+fernandomatsuo-moduscreate@users.noreply.github.com>
2025-03-20 15:00:57 -0300

    Update main.tf

--- terraform/aws/terraform_no_issues/main.tf
+++ terraform/aws/terraform_no_issues/main.tf
@@ -9,42 +9,33 @@ provider "aws" {
 
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = "modus-create-s3-demo-2024-09-15-ni"
+
   tags = {
     Name        = "modus bucket demo"
     Environment = "dev"
   }
 }
 
-resource "aws_s3_bucket_versioning" "s3_bucket_versioning" {
-  bucket = aws_s3_bucket.s3_bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+# resource "aws_s3_bucket_public_access_block" "s3_bucket_public_access_block" {
+#   bucket                  = aws_s3_bucket.s3_bucket.id
+#   block_public_acls       = true
+#   block_public_policy     = true
+#   ignore_public_acls      = true
+#   restrict_public_buckets = true
+# }
 
-#resource "aws_s3_bucket_public_access_block" "s3_bucket_public_access_block" {
-#  bucket                  = aws_s3_bucket.s3_bucket.id
-#  block_public_acls       = true
-#  block_public_policy     = true
-#  ignore_public_acls      = true
-#  restrict_public_buckets = true
-#}
+# resource "aws_kms_key" "kms_key" {
+#   description         = "This key is used to encrypt S3 bucket objects"
+#   enable_key_rotation = true
+# }
 
-resource "aws_kms_key" "kms_key" {
-  description         = "This key is used to encrypt S3 bucket objects"
-  enable_key_rotation = true
-}
+# resource "aws_s3_bucket_server_side_encryption_configuration" "s3_bucket_server_side_encryption_configuration" {
+#   bucket = aws_s3_bucket.s3_bucket.id
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "s3_bucket_server_side_encryption_configuration" {
-  bucket = aws_s3_bucket.s3_bucket.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.kms_key.arn
-      sse_algorithm     = "aws:kms"
-    }
-  }
-}
-
-
-
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       kms_master_key_id = aws_kms_key.kms_key.arn
+#       sse_algorithm     = "aws:kms"
+#     }
+#   }
+# }
